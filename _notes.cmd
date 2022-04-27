332b534e8074b2dedea3516ad5e3df6cda0d021d
Joseph Higaki <josephhigaki@hotmail.com>
2022-04-27 13:08:04 +0200

    updated notebookresults  and notes.cmd commands

--- _notes.cmd
+++ _notes.cmd
@@ -5,10 +5,10 @@ rem /***********activate conda **************************/
 C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
 
 rem /***********create the environment ******************/
-conda env create --prefix C:\Users\josep\OneDrive\Documentos\projects\UdacityDeng\UDENG_L03_E02_IaC\IaC-101-env --file environment.yml
+conda env create --prefix C:\Users\josep\OneDrive\Documentos\projects\UdacityDeng\UDENG_L03_E03_IngestParallelFiles_Redshift\IngestParallelFiles-Redshift-env --file environment.yml
 
 rem /***********activate the environment ******************/
-conda activate C:\Users\josep\OneDrive\Documentos\projects\UdacityDeng\UDENG_L03_E02_IaC\IaC-101-env
+conda activate C:\Users\josep\OneDrive\Documentos\projects\UdacityDeng\UDENG_L03_E03_IngestParallelFiles_Redshift\IngestParallelFiles-Redshift-env
 
 rem /***********DE - activate the environment ******************/
 conda deactivate
@@ -17,11 +17,11 @@ rem /***********when requirements need updated environment **********/
 conda env update --file environment.yml --prune
 
 rem /*********** remove environment if needed  **********/
-conda env remove --prefix C:\Users\josep\OneDrive\Documentos\projects\UdacityDeng\UDENG_L03_E02_IaC\IaC-101-env
+conda env remove --prefix C:\Users\josep\OneDrive\Documentos\projects\UdacityDeng\UDENG_L03_E03_IngestParallelFiles_Redshift\IngestParallelFiles-Redshift-env
 
 rem /*********** pynotepbook kernel to use  **********/
-C:\Users\josep\OneDrive\Documentos\projects\UdacityDeng\UDENG_L03_E02_IaC\IaC-101-env\python.exe
-~\OneDrive\Documentos\projects\UdacityDeng\UDENG_L03_E02_IaC\IaC-101-env\python.exe
+C:\Users\josep\OneDrive\Documentos\projects\UdacityDeng\UDENG_L03_E03_IngestParallelFiles_Redshift\IngestParallelFiles-Redshift-env\python.exe
+~\OneDrive\Documentos\projects\UdacityDeng\UDENG_L03_E03_IngestParallelFiles_Redshift\IngestParallelFiles-Redshift-env\python.exe
 
 rem /*********** git notess ******************/
 git init 
@@ -29,5 +29,5 @@ git add .
 git commit -m "Initial commit"
 git branch -m master main
 git status
-git remote add origin git@github.com:joseph-higaki/UDataEng_L03E02_IaC-101.git
+git remote add origin git@github.com:joseph-higaki/UDataEng_L03E03_IngestParallelFiles_Redshift.git
 git push -u origin main
